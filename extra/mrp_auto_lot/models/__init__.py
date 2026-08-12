23bd414d54a980be3887cf9a07efa477b2de73d1
jponcema <jponce@innatos.com.mx>
2026-08-12 12:46:36 -0600

    feat: Remove obsolete MRP auto lot module and related files

--- extra/mrp_auto_lot/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import models
-from . import res_config_settings
-from . import stock_lot
-from . import mrp_production
--- extra/mrp_auto_lot/models/models.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api, _
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    is_formulated = fields.Boolean(
-        string='Producto Formulado',
-        help='Será usado para determinar la fecha de caducidad de un producto fabricado con este componente.'
-    )
